cb1049cd3b8fc420467f9c879fc1d8957b194ec0
Makhavhu7 <166398447+Makhavhu7@users.noreply.github.com>
2026-03-01 15:34:53 +0200

    New Changes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZoJnibrBnG7-OCzBv65Y8e1t8hAbFee8",
-  authDomain: "tutapp-88bf0.firebaseapp.com",
-  projectId: "tutapp-88bf0",
-  storageBucket: "tutapp-88bf0.firebasestorage.app",
-  messagingSenderId: "999034904150",
-  appId: "1:999034904150:web:7499ef525b430d7fd6e5f7",
-  measurementId: "G-DEYC1VDELW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
